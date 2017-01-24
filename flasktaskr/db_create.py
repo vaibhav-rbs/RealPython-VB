14104a9c2e9e6506a4c847c8518da224f5f0f793
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-01-24 00:13:24 -0800

    working flasktasker

--- flasktaskr/db_create.py
+++ flasktaskr/db_create.py
@@ -6,16 +6,16 @@ with sqlite3.connect(DATABASE_PATH) as connection:
     c = connection.cursor()
 
     c.execute("""
-            CREATE TABLE task(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
             status INTEGER NOT NULL)
     """)
     c.execute("""
-            INSERT INTO task(name,due_date,priority,status)
+            INSERT INTO tasks(name,due_date,priority,status)
             VALUES('Finish this tutorial','01/23/2017',10,1)
     """)
     c.execute("""
-            INSERT INTO task(name,due_date,priority,status)
+            INSERT INTO tasks(name,due_date,priority,status)
             VALUES('Finish Real Python Course 2','02/02/2017',10,1)
     """)
 
