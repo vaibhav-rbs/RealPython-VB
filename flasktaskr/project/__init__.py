ebf97a4e84f5f28c1c96ba2e1937d4e486f27cd9
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-02-05 00:50:04 -0800

    added logging

--- flasktaskr/project/__init__.py
+++ flasktaskr/project/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -13,3 +14,22 @@ from project.tasks.views import tasks_blueprint
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    now = datetime.datetime.now()
+    r = request.url
+    with open('error.log','a') as f:
+        current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+        f.write("\n 404 error at {}: {}".format(current_timestamp, r))
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    now = datetime.datetime.now()
+    r = request.url
+    with open('error.log','a') as f:
+        current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+        f.write("\n 500 error at {}: {}".format(current_timestamp, r))
+    return render_template('500.html'), 500
