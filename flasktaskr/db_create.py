ed3f76a0ee98a71e91571138c0e49c51f5346348
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-02-04 23:30:30 -0800

    encrypted password

--- flasktaskr/db_create.py
+++ flasktaskr/db_create.py
@@ -1,11 +1,24 @@
-from views import db
-from models import *
+# db_create.py
+
+
 from datetime import date
 
+from project import db
+from project.models import Task, User
+
+# create the database and the db table
 db.create_all()
 
+# insert data
+db.session.add(
+    User("admin", "ad@min.com", "admin", "admin")
+)
+db.session.add(
+    Task("Finish this tutorial", date(2015, 3, 13), 10, 1 ,date(2015, 2, 13), 1)
+)
+db.session.add(
+    Task("Finish Real Python", date(2015, 3, 13), 10, 1, date(2015, 2, 13),  1)
+)
 
-#db.session.add(Task("Finish this tutorial", str(date(2017,1,24)), 10,1))
-#db.session.add(Task("Finish this book", str(date(2017,2,9)), 10,1))
-
+# commit the changes
 db.session.commit()
