8de7ba677e9486c48e6a4ad864f2f3621bcbcb30
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-02-06 00:48:43 -0800

    added missing tests

--- flasktaskr/tests/test_users.py
+++ flasktaskr/tests/test_users.py
@@ -4,9 +4,10 @@
 import os
 import unittest
 
-from project import app, db
+from project import app, db, bcrypt
 from project._config import basedir
 from project.models import User
+
 
 TEST_DB = 'test.db'
 
@@ -21,10 +22,13 @@ class UsersTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
+
+        self.assertEquals(app.debug, False)
 
     # executed after each test
     def tearDown(self):
@@ -51,7 +55,11 @@ class UsersTests(unittest.TestCase):
         return self.app.get('logout/', follow_redirects=True)
 
     def create_user(self, name, email, password):
-        new_user = User(name=name, email=email, password=password)
+        new_user = User(
+            name=name,
+            email=email,
+            password=bcrypt.generate_password_hash(password)
+        )
         db.session.add(new_user)
         db.session.commit()
 
@@ -64,6 +72,10 @@ class UsersTests(unittest.TestCase):
             status='1'
         ), follow_redirects=True)
 
+
+    ###############
+    #### tests ####
+    ###############
 
     def test_users_can_register(self):
         new_user = User("michael", "michael@mherman.org", "michaelherman")
