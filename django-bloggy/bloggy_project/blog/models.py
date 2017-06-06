ef436ddf2764e17fcb62c960d667e706dd61817c
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-06-06 15:52:02 -0700

    WIP commit

--- django-bloggy/bloggy_project/blog/models.py
+++ django-bloggy/bloggy_project/blog/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-
 class Post(models.Model):
-	created_at = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
 	title = models.CharField(max_length=100)
-	conntent = models.TextField()
+	content = models.TextField()
+
+def __str__(self):
+	return "{0}/{1}/{2}/{3}\n".format(self.id, self.created_at,self.title,self.content)
