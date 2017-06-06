25ec2a384d8180b3751fb780d31d665feb58c4f5
vaibhav-rbs <vaibhav@ixsystems.com>
2017-06-05 19:52:35 -0700

    WIP commit

--- django-bloggy/bloggy_project/bloggy_project/settings.py
+++ django-bloggy/bloggy_project/bloggy_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local Apps
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'bloggy_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'bloggy.db'),
     }
 }
 
