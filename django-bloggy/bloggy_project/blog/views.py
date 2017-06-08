e97b2f89b373a3932e183c6ebf138a289ed0cb83
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-06-08 00:52:09 -0700

    on page 517 RealPython part2

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -15,7 +15,9 @@ def index(request):
     return HttpResponse(t.render(c))
 
 def post(request, post_url):
-    single_post = get_object_or_404(Post,title=post_url.replace('_',' '))
+    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
+    single_post.views += 1
+    single_post.save()
     t = loader.get_template('blog/post.html')
     c = Context({ 'single_post': single_post })
     return HttpResponse(t.render(c))
