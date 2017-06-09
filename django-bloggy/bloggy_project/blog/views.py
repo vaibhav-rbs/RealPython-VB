edec1b9a90d37a5d93095ed62517da60d835ef16
vaibhav-rbs <vaibhav.rbs@gmail.com>
2017-06-08 23:48:48 -0700

    commited what I commited for on page 522. (RP2)

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -4,13 +4,19 @@ from django.shortcuts import get_object_or_404
 
 from blog.models import Post
 
+def encode_url(url):
+    return url.replace(' ', '_')
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
+    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('blog/index.html')
-    context_dict= Context({ 'latest_posts' : latest_posts })
+    context_dict= Context({ 'latest_posts' : latest_posts,
+                            'popular_posts':popular_posts})
     for post in latest_posts:
-        post.url = post.title.replace(' ', '_')
+        post.url = encode_url(post.title)
+    for popular_post in popular_posts:
+        popular_post.url = encode_url(popular_post.title)
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
