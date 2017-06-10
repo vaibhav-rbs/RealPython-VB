ce0f4d8278d8620fb0ade018625ec99970bd0c6a
vaibhav-rbs <vaibhav@ixsystems.com>
2017-06-09 17:07:33 -0700

    completed bloggy part 2 on Page 538, RP2

--- django-bloggy/bloggy_project/blog/views.py
+++ django-bloggy/bloggy_project/blog/views.py
@@ -5,28 +5,26 @@ from django.shortcuts import get_object_or_404, render, redirect
 from blog.models import Post
 from blog.forms import PostForm
 
-def encode_url(url):
-    return url.replace(' ', '_')
+def get_popular_posts():
+    popular_posts = Post.objects.order_by('-views')[:5]
+    return popular_posts
 
 def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
-    popular_posts = Post.objects.order_by('-views')[:5]
     t = loader.get_template('blog/index.html')
-    context_dict= Context({ 'latest_posts' : latest_posts,
-                            'popular_posts':popular_posts})
-    for post in latest_posts:
-        post.url = encode_url(post.title)
-    for popular_post in popular_posts:
-        popular_post.url = encode_url(popular_post.title)
+    context_dict = { 'latest_posts' : latest_posts, 'popular_posts': get_popular_posts()}
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
-def post(request, post_url):
-    single_post = get_object_or_404(Post, title=post_url.replace('_', ' '))
+def post(request, slug):
+    single_post = get_object_or_404(Post, slug=slug)
     single_post.views += 1
     single_post.save()
     t = loader.get_template('blog/post.html')
-    c = Context({ 'single_post': single_post })
+    c = Context({
+        'single_post': single_post,
+        'popular_posts': get_popular_posts()
+        })
     return HttpResponse(t.render(c))
 
 def add_post(request):
